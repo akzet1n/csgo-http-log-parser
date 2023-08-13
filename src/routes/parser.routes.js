7d5ad9ff5e887d671dacd86c54dffab573916c1c
Roberto Videla <robertovidelac@gmail.com>
2023-08-13 14:01:00 -0400

    redo

--- src/routes/parser.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { parseData } from "../controllers/parser.controller.js";
-
-const router = express.Router();
-
-router.get("/", parseData);
-
-export default router;
