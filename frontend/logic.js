9b694afa47a2186fa96416e3134fab153e5b4a4c
Roberto Videla <robertovidelac@gmail.com>
2023-08-23 19:46:46 -0300

    frontend refactor

--- frontend/logic.js
+++ frontend/logic.js
@@ -1,85 +1,34 @@
-const divElement = (className) => {
-    const div = document.createElement("div");
-    if (className)
-        div.classList.add(className);
-    return div;
-};
-
-const pElement = (content, className) => {    
-    const p = document.createElement("p");
-    if (className)
-        p.classList.add(className);
-    p.textContent = content;
-    return p;
-};
-
-const logs = document.getElementById("logs");
-
 const addContent = (data) => {
 
-    const div = divElement("log");
-    let round;
+    const logs = document.getElementById("logs");
+    var log = document.createElement("p");
+    log.classList.add("log");
+
+    const status = document.getElementById("status");
+    status.classList.add("hide");
 
     if (data.round) {
-        round = pElement("Round " + data.round + " | ");
-        div.appendChild(round);
+        log.innerHTML = `Round ${data.round} | `;
         if (data.type === "kill") {
-            const killer = pElement(data.killer, data.killerTeam === "CT" ? "ct" : "terrorist");
-            div.appendChild(killer);
-            var text = pElement("killed");
-            div.appendChild(text);
-            const victim = pElement(data.victim, data.victimTeam === "CT" ? "ct" : "terrorist");
-            div.appendChild(victim);
-            text = pElement("with");
-            div.appendChild(text);
-            const weapon = pElement(data.weapon);
-            div.appendChild(weapon);
-            if (data.extras) {
-                const extras = pElement("(" + data.extras + ")");
-                div.appendChild(extras);
-            }
-        } else if (data.type === "bomb") {
-            const player = pElement(data.player, data.team === "CT" ? "ct": "terrorist");
-            div.appendChild(player);
-            const message = pElement(data.message);
-            div.appendChild(message);
-        } else if (data.type === "say") {
-            const player = pElement(data.player, data.team === "CT" ? "ct": "terrorist");
-            div.appendChild(player);
-            const text = pElement("wrote:");
-            div.appendChild(text);
-            const message = pElement(data.message);
-            div.appendChild(message);
-        } else if (data.type === "player") {
-            const player = pElement(data.player, data.team === "CT" ? "ct": "terrorist");
-            div.appendChild(player);
-            const message = pElement(data.message);
-            div.appendChild(message);
-        } else if (data.type === "gg" || data.type === "world" || data.type === "freezetime" || data.type === "round" || data.type === "pause") {
-            const p = pElement(data.message);
-            div.appendChild(p);
-        } else if (data.type === "status") {
-            const text = pElement("Match score:");
-            div.appendChild(text);
-            const ctText = pElement("CT", "ct");
-            div.appendChild(ctText);
-            const ctScore = pElement(data.ctScore);
-            div.appendChild(ctScore);
-            const sep = pElement("vs");
-            div.appendChild(sep);
-            const tScore = pElement(data.tScore);
-            div.appendChild(tScore);
-            const tText = pElement("T", "terrorist");
-            div.appendChild(tText);
-        }
+            log.innerHTML += `<span class=${data.killerTeam === "CT" ? "ct" : "t"}>${data.killer}</span> killed `;
+            log.innerHTML += `<span class=${data.victimTeam === "CT" ? "ct" : "t"}>${data.victim}</span> with `;
+            log.innerHTML += `${data.weapon} `;
+            if (data.extras)
+                log.innerHTML += `(${data.extras})`;
+        } else if (data.type === "bomb" || data.type === "player")
+            log.innerHTML += `<span class=${data.team === "CT" ? "ct": "t"}>${data.player}</span> ${data.message}`;
+        else if (data.type === "say")
+            log.innerHTML += `<span class=${data.team === "CT" ? "ct": "t"}>${data.player}</span> wrote: ${data.message}`;
+        else if (data.type === "gg" || data.type === "world" || data.type === "freezetime" || data.type === "round" || data.type === "pause")
+            log.innerHTML += `${data.message}`;
+        else if (data.type === "status")
+            log.innerHTML += `Match score: <span class="ct">CT</span> (${data.ctScore}) vs (${data.tScore}) <span class="t">T</span>`;
+    } else {
+        if (data.type === "connection")
+            log.innerHTML += `${data.message}`;
     }
 
-    if (data.type === "connection") {
-        const p = pElement(data.message);
-        div.appendChild(p);
-    }
-
-    logs.appendChild(div);
+    logs.appendChild(log);
 
 };
 
